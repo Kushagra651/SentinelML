2de9ee84d1e2d399c18fca7ee3125f3f364d3862
kushagra651 <kushagrabhargava93@gmail.com>
2026-08-12 11:31:17 +0530

    fix: CI DB schema apply step for airflow database

--- init_db.sql
+++ init_db.sql
@@ -8,7 +8,6 @@
 
 -- Also create the airflow database if it doesn't exist
 -- (Airflow init handles its own schema, we just need the DB)
-SELECT 'CREATE DATABASE airflow'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'airflow')\gexec
 GRANT ALL PRIVILEGES ON DATABASE airflow TO mluser;
 
